40839b54f85be1eaf9ee9cc07316f248dece1b51
alex gong <110605035@qq.com>
2019-05-01 10:23:59 +0800

    add bash shell

---
+++ runall.sh
@@ -0,0 +1,2 @@
+python augmentateMark.py /media/alex/1T/source/opencv/alex/4imgaug/img2cuted /home/alex/source/imgaug/images/to
+
---
+++ runsample.sh
@@ -0,0 +1,2 @@
+python augmentateMark.py /home/alex/source/imgaug/images/from /home/alex/source/imgaug/images/to
+
